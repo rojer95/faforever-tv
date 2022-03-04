bbece79ba895ebe98333d654bda81e37c482bbfd
rojer <rojerchen@qq.com>
2022-03-04 12:50:37 +0800

    fix: :bug: 动态图标不能热更新问题

--- src/components/Musicing.tsx
+++ src/components/Musicing.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import ImageSequence from 'react-native-image-sequence';
 import styled from 'styled-components/native';
+import {Image as RNImage} from 'react-native';
 
 const Image = styled(ImageSequence)<Props>`
   width: ${props => props.width}px;
   height: ${props => props.height}px;
 `;
+
 interface Props {
   height: number;
   width: number;
 }
+
+const i1 = require('../assets/gif_musicing_1.png');
+const i2 = require('../assets/gif_musicing_2.png');
+const i3 = require('../assets/gif_musicing_3.png');
+const i4 = require('../assets/gif_musicing_4.png');
+const i5 = require('../assets/gif_musicing_5.png');
+
 export default ({width, height}: Props) => {
   return (
-    <Image
-      images={[
-        require('../assets/gif_musicing_1.png'),
-        require('../assets/gif_musicing_2.png'),
-        require('../assets/gif_musicing_3.png'),
-        require('../assets/gif_musicing_4.png'),
-        require('../assets/gif_musicing_5.png'),
-      ]}
-      framesPerSecond={8}
-      width={width}
-      height={height}
+    <RNImage
+      style={{width: width, height: height}}
+      source={require('../assets/music.png')}
     />
   );
 };
